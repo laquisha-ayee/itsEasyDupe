c5c111d93fa4ba24216bf74f32092ca025b26336
laquisha-ayee <rayelaquisha@gmail.com>
2025-08-20 19:38:34 -0700

    init update

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL', 'sqlite:///dev.db'
-    ).replace('postgres://', 'postgresql://')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
