21c163de6a67f135ba991ef1448ac2335d86afce
laquisha-ayee <rayelaquisha@gmail.com>
2025-08-20 18:30:43 -0700

    text error

--- app/seeds/favorites.py
+++ app/seeds/favorites.py
@@ -36,7 +36,7 @@ def seed_favorites():
 
 def undo_favorites():
     if environment == "production":
-        db.session.execute(text(f"TRUNCATE table {SCHEMA}.favorites RESTART IDENTITY CASCADE;"))
+        db.session.execute(f"TRUNCATE table {SCHEMA}.favorites RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM favorites"))
 
